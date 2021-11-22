5432786bfa8d4260a21896f03e9064f825c3df2d
infinite-ace <angelkukushev@infinitelambda.com>
2021-11-22 19:28:06 +0200

    Improves lift travel method

--- lift.py
+++ lift.py
@@ -1,9 +1,12 @@
+import time
+from termcolor import colored
+
 class Lift:
     emergency_number = ""
     model = ""
     # Zero floor represents our ground floor
     current_level = 0
-    max_floors = 6
+    max_floors = 5
 
     def __init__(self, model, current_level, emergency_number):
         self.model = model
@@ -12,8 +15,23 @@ class Lift:
 
     def travel(self, destination_level):
         if destination_level > self.current_level:
-            print("Going up")
+
+            print("Going up. Floors to travel: ",  abs(destination_level - self.current_level))
+
+            time.sleep(abs(destination_level - self.current_level))
+
+            print(colored('Arrived', 'green'))
+
         elif destination_level < self.current_level:
-            print("Going down")
+
+            print("Going down. Floors to travel: ",  abs(destination_level - self.current_level))
+
+            time.sleep(abs(destination_level - self.current_level))
+
+            print(colored('Arrived', 'green'))
 
         self.current_level = destination_level
+
+        print(colored('Caution! The door is opening and closing for 2 seconds.', 'yellow'))
+
+        time.sleep(2)
